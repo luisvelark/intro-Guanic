59f165d61b46f74eb3f9b3ae509be08045bcb50c
luisito <velark07@gmail.com>
2020-08-08 19:43:18 -0300

    funcionalidad del btn petroleo

---
+++ testing-01/js/petroleo.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", start);
+
+function start() {
+  document.getElementById("petro").addEventListener("click", actionPetro);
+}
+
+function actionPetro() {
+  hiddenSection();
+  exchangeImg();
+}
+
+function hiddenSection() {
+  let section = document.getElementById("section3");
+  section.style.display = "none";
+}
+
+function exchangeImg() {
+  let img1 = document.getElementById("img1");
+  img1.setAttribute("src", "img/02.jpg");
+
+  let img2 = document.getElementById("img2");
+  img2.setAttribute("src", "img/01.jpg");
+}
